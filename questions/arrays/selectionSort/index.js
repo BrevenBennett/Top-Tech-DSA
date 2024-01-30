16271a298e29336b7e5292f230e5ca90ec079d4b
BrevenBennett <Breven.stars@gmail.com>
2024-01-30 09:07:48 -0600

    learned selection sort algorithm

--- questions/arrays/selectionSort/index.js
+++ questions/arrays/selectionSort/index.js
@@ -1,16 +1,30 @@
 /** selection Sort **
- * 
+ *
  * Sort the 'nums' array using selection sort.
- *  
+ *
  * @example
  * selectionSort([3, 2, 1]) -> [1, 2, 3]
  * selectionSort([0, 12, 8]) -> [0, 8, 12]
  * selectionSort([3, 4, 0, 2]) -> [0, 2, 3, 4]
- * 
+ *
  */
 
- const selectionSort = (nums) => {
+const selectionSort = (nums) => {
+  for (let i = 0; i < nums.length; ++i) {
+    let min = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = nums[i];
+      nums[i] = nums[min];
+      nums[min] = temp;
+    }
+  }
 
-}
+  return nums;
+};
 
 module.exports = selectionSort;
