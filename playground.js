45197189d898286f4e30f733175afccf777e8143
BrevenBennett <Breven.stars@gmail.com>
2024-02-01 14:17:02 -0600

    learned how to calculate sum of tree using BFS

--- playground.js
+++ playground.js
@@ -1,22 +1,40 @@
 class Node {
-  constructor(val, next) {
+  constructor(val) {
     this.val = val;
-    this.next = next;
-  }
-
-  print() {
-    let length = 0;
-    let curr = this;
-
-    while (curr) {
-      length++;
-      curr = curr.next;
-    }
-
-    console.log(length);
+    this.left = null;
+    this.right = null;
   }
 }
 
-const head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(4567, null)))));
+const A = new Node(1);
+const B = new Node(2);
+const C = new Node(3);
+const D = new Node(4);
+const E = new Node(5);
+const F = new Node(6);
+const G = new Node(7);
 
-head.print()
+A.left = B;
+A.right = C;
+B.left = D;
+B.right = E;
+C.left = F;
+C.right = G;
+
+const sumOfTree = (root) => {
+  const queue = [root];
+  let sum = 0;
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    sum += current.val;
+
+    if (current.left !== null) queue.push(current.left);
+    if (current.right !== null) queue.push(current.right);
+  }
+
+  return sum;
+};
+
+console.log(sumOfTree(A))
